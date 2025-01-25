62e4a2e5a053a363db5d651e9bf97af877ec5e8b
Syed Hassaan Ali <syedhassanali880@gmail.com>
2025-01-25 20:19:49 +0500

    Changed tools' icons.

--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -1,58 +1,73 @@
 import React from 'react';
-import adodotnet from './adonet.png';
-import entityframrworkcore from './entityframeworkcore.png';
-import notepadplusplus from './notepadplusplus.png';
-import postman from './postman.png';
-import react from './react.png';
-import restapi from './restapi.png';
-import slack from './slack.png';
-import dotnetcore from './dotnetcore.png';
-import sqlserver from './sqlserver.png';
-import githubdesktop from './GitHubDesktop.png';
-import csharp from './c#.png';
-import javascript from './JS.png';
-import vscode from './vscode.png';
-import visualstudio from './visualstudio.png';
 
 const Tools = () => {
   return (
     <div class='text-center mt-8'>
-  <h1 class="font-bold">TECHNOLOGIES I USE</h1>
-  <div class="flex flex-wrap justify-center items-center gap-8 mt-8">
-    <div class="flex flex-col items-center">
-      <i class="fa-brands fa-react text-white-400 text-7xl px-6"></i>
-      <h6 class="mt-2 text-center text-white">React.js</h6>
+      <h1 class="font-bold text-2xl text-white">TECHNOLOGIES I USE</h1>
+      <div class="flex flex-wrap justify-center items-center gap-8 mt-8">
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+        <h6 class="mt-2 text-center text-white">React.js</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original.svg" />
+        <h6 class="mt-2 text-center text-white">Microsoft SQL Server</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <i class="fa-brands fa-github text-white text-[5rem]"></i>
+        <h6 class="mt-2 text-center text-white">Github</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <i class="fa-brands fa-microsoft text-white text-[5rem]"></i>
+        <h6 class="mt-2 text-center text-white">Microsoft Entity Framework Core</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <i class="fa-brands fa-microsoft text-white text-[5rem]"></i>
+        <h6 class="mt-2 text-center text-white">Microsoft ADO.NET</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/visualstudio/visualstudio-original.svg"/>
+        <h6 class="mt-2 text-center text-white">Microsoft Visual Studio</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg" />
+        <h6 class="mt-2 text-center text-white">Microsoft Visual Studio Code</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
+        <h6 class="mt-2 text-center text-white">C#</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg" />
+        <h6 class="mt-2 text-center text-white">.NET Core</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+        <h6 class="mt-2 text-center text-white">Javascript</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+        <h6 class="mt-2 text-center text-white">HTML</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+        <h6 class="mt-2 text-center text-white">Tailwind CSS</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" />
+        <h6 class="mt-2 text-center text-white">Bootstrap</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactbootstrap/reactbootstrap-original.svg" />
+        <h6 class="mt-2 text-center text-white">React Bootstrap</h6>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" />
+        <h6 class="mt-2 text-center text-white">Postman</h6>
+      </div>
     </div>
-    <div class="flex flex-col items-center">
-      <i class="fa-duotone fa-solid fa-database text-white-400 text-7xl px-6"></i>
-      <h6 class="mt-2 text-center text-white">Microsoft SQL Server</h6>
-    </div>
-    <div class="flex flex-col items-center">
-      <i class="fa-brands fa-github text-white-400 text-7xl px-6"></i>
-      <h6 class="mt-2 text-center text-white">Github</h6>
-    </div>
-    <div class="flex flex-col items-center">
-      <i class="fa-brands fa-js text-white-400 text-7xl px-6"></i>
-      <h6 class="mt-2 text-center text-white">Javascript</h6>
-    </div>
-    <div class="flex flex-col items-center">
-      <i class="fa-brands fa-microsoft text-white-400 text-7xl px-6"></i>
-      <h6 class="mt-2 text-center text-white">Microsoft .NET Core</h6>
-    </div>
-    <div class="flex flex-col items-center">
-      <i class="fa-brands fa-microsoft text-white-400 text-7xl px-6"></i>
-      <h6 class="mt-2 text-center text-white">Microsoft Entity Framework Core</h6>
-    </div>
-    <div class="flex flex-col items-center">
-      <i class="fa-brands fa-microsoft text-white-400 text-7xl px-6"></i>
-      <h6 class="mt-2 text-center text-white">Microsoft Visual Studio</h6>
-    </div>
-    <div class="flex flex-col items-center">
-      <i class="fa-solid fa-code text-white-400 text-7xl px-6"></i>
-      <h6 class="mt-2 text-center text-white">C#</h6>
-    </div>
-  </div>
 </div>
+
 
   );
 };
